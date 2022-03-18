88e223d19fbe369ad2943469da7b498547eaecd7
Suma <sumaperam.chinnu@gmail.com>
2022-03-18 14:51:01 +0530

    moved into localrepo

---
+++ testcode.tf
@@ -0,0 +1,42 @@
+{
+    "name": "terraform",
+    "version": "0.1.0",
+    "schemaVersion": "v1.0.0-WD",
+    "parameters": {
+        "backend": {
+            "type": "boolean",
+            "defaultValue": false,
+            "destination": {
+                "env": "TF_VAR_backend"
+            }
+        }
+    },
+    "invocationImages": [
+        {
+        "imageType": "docker",
+        "image": "cnab/terraform:latest"
+        }
+    ],
+    "credentials": {
+        "tenant_id": {
+            "env": "TF_VAR_tenant_id"
+        },
+        "client_id": {
+            "env": "TF_VAR_client_id"
+        },
+        "client_secret": {
+            "env": "TF_VAR_client_secret"
+        },
+        "subscription_id": {
+            "env": "TF_VAR_subscription_id"
+        },
+        "ssh_authorized_key": {
+            "env": "TF_VAR_ssh_authorized_key"
+        }
+    },
+    "actions": {
+        "status": {
+            "modifies": true
+        }
+    }
+}
